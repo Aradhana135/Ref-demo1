75c52c6180c56a57078c033c2747d8aaa5195f02
Aradhana135 <aradhana.kumari@creolestudios.com>
2022-07-26 18:31:05 +0530

    forwarding-ref-demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FRParentInput from './Components/FRParentInput';
+
+function App() {
+  return (
+   <>
+   <FRParentInput/>
+   </>
+  );
+}
+
+export default App;
